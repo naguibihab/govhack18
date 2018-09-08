624792b75a31fb74dd8fb8157bcb86a90bf64aa0
Naguib Ihab <nick@driveyello.com>
2018-09-09 09:12:47 +1000

    Adding some more stuff in lambda & cleaning up

--- lambda-api/main.js
+++ lambda-api/main.js
@@ -1,8 +1,18 @@
 const ApiBuilder = require('claudia-api-builder'),
     api = new ApiBuilder();
+const request = require('request');
 
 module.exports = api;
 
-api.get('/hello', function() {
-    return 'hello world';
+api.get('/native-land/{lat}/{lng}', function(req) {
+    var lat = req.pathParams.lat;
+    var lng = req.pathParams.lng;
+
+    var url ='https://native-land.ca/api/index.php?maps=languages&position='+lat+','+lng;
+
+	request(url, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	        console.log(body) // Print the google web page.
+	     }
+	})
 });
